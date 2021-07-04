cd6e3123d975e7c99c10804271682bdb95486e1b
James-VT <jd_tanner@hotmail.co.uk>
2021-07-04 23:10:44 +0000

    restructures js file to try to get it to work. It still doesn't

--- assets/js/battlefields.js
+++ assets/js/battlefields.js
@@ -1,9 +1,10 @@
 // The below is to test whether this file in linked properly to the index.html file
 // and its button elements
 
+let button = document.getElementsByClassName("button");
+
 function buttonTest() {
     console.log("button clicked");
 }
 
-let button = document.getElementsByClassName("button");
 button.addEventListener('click', buttonTest);
