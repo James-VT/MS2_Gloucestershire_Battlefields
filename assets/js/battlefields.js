b089a79c8925250ba9def43ba3c8a4e308c00c21
James-VT <jd_tanner@hotmail.co.uk>
2021-07-14 22:14:11 +0000

    writes: Script for the event listeners on the markers

--- assets/js/battlefields.js
+++ assets/js/battlefields.js
@@ -18,28 +18,38 @@ $(document).ready(function() {
     })
 });
 
+/** The const below is for the map's centre coordinates. I've called this outside of the function itself as calling it inside failed to work. */
+
 const properties = {"lat": 51.864445, "lng":-2.244444}
 
-/** The code below is an attempt to make Google Maps API work - credit goes to Eamonn Smyth, a fellow student at Code Institute, for his excellent guide from which much code is taken for this section */
+/** The code below is an attempt to make Google Maps API work - credit goes to Eamonn Smyth, a fellow student at Code Institute, for his excellent guide from which much code is taken for this section.
+ * I have changed a number of parts, particularly the variable names.
+ */
 /** The function below creates the map */
+
 function myMap() {
 
     /** The below code is to define the map's properties */
+
     const mapProperties = {
 
         /**The centre property below is where we centre our map - in our case, the city of Gloucester
          * We're using the .LatLng constructor here */
+
         center: properties,
 
         /**The zoom property is set to 8 as we're covering a fairly small area for this site */
+
         zoom: 8,
     };
 
     /** This is what creates the map itself - it goes in the div with the ID of "map" 
      * then it checks the properties variable object for the nod of what we want in it */
+
     const map = new google.maps.Map(document.getElementById("map"), mapProperties);
 
     /** Below we're adding some markers. Here are the coordinates for them, in an array of objects */
+
     const battleMarks = [{"lat": 51.9923, "lng": -2.1580, "name": "Tewkesbury"},
                          {"lat": 51.8472, "lng": -2.0978, "name": "Crickley Hill"},
                          {"lat": 51.6444, "lng": -2.2011, "name": "Beverston Castle"},
@@ -48,20 +58,30 @@ function myMap() {
                          {"lat": 51.688611, "lng": -2.456944, "name": "Berkeley Castle"}];
 
     /** Here's where we loop through the battleMarks objects */
+
     for(let i=0; i < battleMarks.length; i++) {
         const marker = new google.maps.Marker({
             position: new google.maps.LatLng(battleMarks[i].lat, battleMarks[i].lng),
             map: map,
             title: battleMarks[i].name
         });
+        /** This is where the code I got from Eamonn Smyth ends. */
 
         /** This is where we add event listeners to our markers */
+
         marker.addListener("click", () => {
             map.setZoom(9);
             map.setCenter(marker.getPosition());
             console.log("marker listener successful")
+            tewkesburyBattleInfo()
         });
     }
 }
+
+function tewkesburyBattleInfo() {
+    document.getElementById(".battle-title").innerHTML(`Battle of Tewkesbury`)
+}
+
 /** And here we call the map itself */
+
 myMap()
