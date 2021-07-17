876904a806bef506f6d21cbe9b86ed564458c130
James-VT <jd_tanner@hotmail.co.uk>
2021-07-17 23:27:33 +0000

    adds: Adds combatants code to clickers

--- assets/js/battlefields.js
+++ assets/js/battlefields.js
@@ -18,31 +18,37 @@ $(document).ready(function() {
     $("#tewkesbury-button").click(function() {
         $(".battle-title").text(`Battle of Tewkesbury`);
         $(".battle-date").text(`4 May 1471`);
+        $(".battle-combatants").text(`The House of York versus the House of Lancaster`);
     })
 
     $("#crickley-button").click(function() {
         $(".battle-title").text(`Battle of Crickley Hill`);
         $(".battle-date").text(`~3,300 BC`);
+        $(".battle-combatants").text(`Combatants unknown`);
     })
 
     $("#beverston-button").click(function() {
         $(".battle-title").text(`Battle of Beverston Castle`);
-        $(".battle-date").text(`1644 and 1691`);
+        $(".battle-date").text(`1644`);
+        $(".battle-combatants").text(`Royalists versus Parliament`);
     })
 
     $("#stow-button").click(function() {
         $(".battle-title").text(`Battle of Stow-on-the-Wold`);
         $(".battle-date").text(`21 March 1646`);
+        $(".battle-combatants").text(`Royalists versus Parliament`);
     })
 
     $("#deorham-button").click(function() {
         $(".battle-title").text(`Battle of Deorham`);
         $(".battle-date").text(`577 AD`);
+        $(".battle-combatants").text(`West Saxons versus Britons`);
     })
 
     $("#berkeley-button").click(function() {
         $(".battle-title").text(`Battle of Berkeley Castle`);
         $(".battle-date").text(`1326, 1645`)
+        $(".battle-combatants").text(`Hugh Despenser versus Baron Berkeley (1326), Royalists verus Parliament (1645)`);
     })
 });
 
@@ -79,12 +85,12 @@ function myMap() {
 
     /** Below we're adding some markers. Here are the coordinates for them, in an array of objects */
 
-    var battleMarks = [{"lat": 51.9923, "lng": -2.1580, "name": "Battle of Tewkesbury", "date": "4 May 1471"},
-                         {"lat": 51.8472, "lng": -2.0978, "name": "Battle of Crickley Hill", "date": "~3,300 BC"},
-                         {"lat": 51.6444, "lng": -2.2011, "name": "Battle of Beverston Castle", "date": "1644 and 1691"},
-                         {"lat": 51.951708, "lng": -1.728297, "name": "Battle of Stow-on-the-Wold", "date": "21 March 1646"},
-                         {"lat": 51.4891, "lng": -2.373706, "name": "Battle of Deorham", "date": "577 AD"},
-                         {"lat": 51.688611, "lng": -2.456944, "name": "Battle of Berkeley Castle", "date": "1326, 1645"}];
+    var battleMarks = [{"lat": 51.9923, "lng": -2.1580, "name": "Battle of Tewkesbury", "date": "4 May 1471", "combatants": "The House of York versus the House of Lancaster"},
+                         {"lat": 51.8472, "lng": -2.0978, "name": "Battle of Crickley Hill", "date": "~3,300 BC", "combatants": "Combatants unknown"},
+                         {"lat": 51.6444, "lng": -2.2011, "name": "Battle of Beverston Castle", "date": "1644", "combatants": "Royalists versus Parliament"},
+                         {"lat": 51.951708, "lng": -1.728297, "name": "Battle of Stow-on-the-Wold", "date": "21 March 1646", "combatants": "Royalists versus Parliament"},
+                         {"lat": 51.4891, "lng": -2.373706, "name": "Battle of Deorham", "date": "577 AD", "combatants": "West Saxons versus Britons"},
+                         {"lat": 51.688611, "lng": -2.456944, "name": "Battle of Berkeley Castle", "date": "1326, 1645", "combatants": "Hugh Despenser versus Baron Berkeley (1326), Royalists versus Parliament (1645)"}];
 
     /** Here's where we loop through the battleMarks objects */
 
@@ -106,6 +112,7 @@ function myMap() {
             console.log("marker listener successful")
             $(".battle-title").text(`${battleMarks[i].name}`);
             $(".battle-date").text(`${battleMarks[i].date}`);
+            $(".battle-combatants").text(`${battleMarks[i].combatants}`);
         });
     }
 }
