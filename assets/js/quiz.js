1c433c012c73cc4ce939cf3cc9b74a7ac135b7bd
James-VT <jd_tanner@hotmail.co.uk>
2021-07-27 23:16:24 +0000

    edit: Moves around hide class to get the buttons to hide/unhide properly

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,10 +1,21 @@
+
+/** Much of the code in this file was inspired by this video tutorial: https://www.youtube.com/watch?v=riDzcEQbX6k&t=379s
+ * I have edited various parts to fit the needs of the project, in particular function and variable names but also how the code works
+ * i.e. we have no need of a "next" button to trigger the next question, as the next question is triggered automatically upon submitting
+ * and answer for the previous question.
+ */
+
 const beginButton = document.getElementById("begin-button")
+const answerButtonsElement = document.getElementById("answer-buttons")
+const questionElement = document.getElementById("quiz-question")
 
 beginButton.addEventListener("click", beginQuiz)
 
 function beginQuiz() {
-    console.log("begun")
+    console.log("begun");
     beginButton.classList.add("hide")
+    answerButtonsElement.classList.remove('hide')
+    questionElement.classList.remove('hide')
 }
 
 function nextQuestion() {
