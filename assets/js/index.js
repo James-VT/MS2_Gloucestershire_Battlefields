3b5828a0a4ef9f1e758add22afc0d1184d3ecdb7
James-VT <jd_tanner@hotmail.co.uk>
2021-08-12 19:51:31 +0000

    edit: Add a comment to in index.js about necessity

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,11 @@
+/** This is the JS file for index.html. Although it may seem odd to have a separate file for just this small snippet of code, I wanted
+ * to avoid inline or embedded JS as I haven't used it anywhere else in this project. The question, of course, is whether I really 
+ * need this bit of code at all if it's to be the only bit of JS (or in this case, JQuery) used in the project. Couldn't I find another 
+ * solution? Well, maybe. But I felt it was necessary to present the intro text as being optional as it is fairly long and takes 
+ * up a lot of the screen, especially on mobile devices. Plus I haven't been able to use as much JQuery as I would've liked on 
+ * this project so I'm taking any excuse to use it.
+*/
+
 $(".text-reveal").click(function() {
     $(".intro-expanded").show(1000, function() {
         $(".text-reveal").addClass("hide")
