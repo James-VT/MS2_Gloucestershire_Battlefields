e0d838580fcbd271daffcb4ffb39877b24d3c456
James-VT <jd_tanner@hotmail.co.uk>
2021-08-20 19:47:22 +0000

    edit: Add more info to the map markers and buttons

--- assets/js/battlefields.js
+++ assets/js/battlefields.js
@@ -54,13 +54,13 @@ function myMap() {
                         "date": "4 May 1471", 
                         "combatants": "The House of York versus the House of Lancaster",
                         "outcome": "Yorkist victory",
-                        "detailed": "The Battle of Tewkesbury, which took place on 4 May 1471, was one of the decisive battles of the Wars of the Roses in England. The forces loyal to the House of Lancaster were completely defeated by those of the rival House of York under their monarch, King Edward IV. The Lancastrian heir to the throne, Edward, Prince of Wales, and many prominent Lancastrian nobles were killed during the battle or executed. The Lancastrian king, Henry VI of England, who was a prisoner in the Tower of London, died or was murdered shortly after the battle. Tewkesbury restored political stability to England until the death of Edward IV in 1483"},
+                        "detailed": "The Battle of Tewkesbury, which took place on 4 May 1471, was one of the decisive battles of the Wars of the Roses in England. The forces loyal to the House of Lancaster were completely defeated by those of the rival House of York under their monarch, King Edward IV. The Lancastrian heir to the throne, Edward, Prince of Wales, and many prominent Lancastrian nobles were killed during the battle or executed. The Lancastrian king, Henry VI of England, who was a prisoner in the Tower of London, died or was murdered shortly after the battle. Tewkesbury restored political stability to England until the death of Edward IV in 1483  at which point his brother, Richard, Duke of Gloucester, was crowned king and ruled as the last monarch of the York dynasty.  These events would inspire many works of fiction in later centuries, including notable television series such as Game of Thrones."},
                          {"lat": 51.8472, "lng": -2.0978, 
                          "name": "Battle of Crickley Hill", 
                          "date": "~3,300 BC", 
                          "combatants": "Combatants unknown",
                          "outcome": "Outcome unknown; settlement defences partially levelled",
-                         "detailed": "Archaeologically speaking, the earliest battle evidence from the British Isles comes from the Neolithic or New Stone Age, when the farmers were first competing for limited resources. Crickley Hill is the earliset know battle that took place in the UK. At Crickley Hill in Gloucestershire, the earthen ramparts of the settlement were attacked, some time around 3300 BC, and their defences partially levelled."},
+                         "detailed": "Archaeologically speaking, the earliest battle evidence from the British Isles comes from the Neolithic or New Stone Age, when the farmers were first competing for limited resources. Crickley Hill is the earliset know battle that took place in what is now the UK. At Crickley Hill in Gloucestershire, the earthen ramparts of the settlement were attacked, some time around 3300 BC, and their defences partially levelled."},
                          {"lat": 51.6444, "lng": -2.2011, 
                          "name": "Battle of Beverston Castle", 
                          "date": "1644",
@@ -72,7 +72,7 @@ function myMap() {
                          "date": "21 March 1646", 
                          "combatants": "Royalists versus Parliament",
                          "outcome": "Parliamentarian victory",
-                         "detailed": "In the spring of 1646, King Charles I of England was getting ever more desperate to hold the Royalist cause together whilst waiting for the long promised relief forces from Ireland, Scotland and France. Sir Jacob Astley took command of the Royalist forces in the west and began to gather up the remnants from the handful of Royalist garrisons still left in the West Midlands to create a new field army. At this point in the war, Royalist morale was low. However, Astley, a stalwart of the Royalist commanders and an experienced soldier, was able to cobble together a force of 3,000. While returning to Oxford he was intercepted and defeated at Stow-on-the-Wold. Some units of parliamentarian soldiers, arranged in regiments, wore uniforms coloured in Venetian Red - this was one of the first wars in which soldiers wore uniform clothing. In the years that followed, this colour would be adopted by all regiments of England's - and later Britain's - army, giving rise to the term 'redcoats.'"},
+                         "detailed": "In the spring of 1646, King Charles I of England was getting ever more desperate to hold the Royalist cause together whilst waiting for the long promised relief forces from Ireland, Scotland and France. Sir Jacob Astley took command of the Royalist forces in the west and began to gather up the remnants from the handful of Royalist garrisons still left in the West Midlands to create a new field army. At this point in the war, Royalist morale was low. However, Astley, a stalwart of the Royalist commanders and an experienced soldier, was able to cobble together a force of 3,000. While returning to Oxford he was intercepted and defeated at Stow-on-the-Wold , which was the last major battle of the first English Civil War. Some units of parliamentarian soldiers, arranged in regiments, wore uniforms coloured in Venetian Red - this was one of the first wars in which soldiers wore uniform clothing. In the years that followed, this colour would be adopted by all regiments of England's - and later Britain's - army, giving rise to the term 'redcoats.'"},
                          {"lat": 51.4891, "lng": -2.373706, 
                          "name": "Battle of Deorham", 
                          "date": "577 AD", 
@@ -123,7 +123,9 @@ function myMap() {
         of the Roses in England. The forces loyal to the House of Lancaster were completely defeated by those of the rival House of York under 
         their monarch, King Edward IV. The Lancastrian heir to the throne, Edward, Prince of Wales, and many prominent Lancastrian nobles were 
         killed during the battle or executed. The Lancastrian king, Henry VI of England, who was a prisoner in the Tower of London, died or was 
-        murdered shortly after the battle. Tewkesbury restored political stability to England until the death of Edward IV in 1483`);
+        murdered shortly after the battle. Tewkesbury restored political stability to England until the death of Edward IV in 1483 at which point 
+        his brother, Richard, Duke of Gloucester, was crowned king and ruled as the last monarch of the York dynasty. These events would inspire many 
+        works of fiction in later centuries, including notable television series such as Game of Thrones.`);
         map.setZoom(9);
         let tewksCoords = {"lat": 51.9923, "lng": -2.1580};
         map.panTo(tewksCoords);
@@ -135,7 +137,7 @@ function myMap() {
         $(".battle-combatants").text(`Combatants unknown`);
         $(".battle-outcome").text(`Outcome unknown; settlement defences partially levelled`);
         $(".battle-detailed").text(`Archaeologically speaking, the earliest battle evidence from the British Isles comes from the Neolithic or New Stone Age, 
-        when the farmers were first competing for limited resources. Crickley Hill is the earliset know battle that took place in the UK. At Crickley Hill in Gloucestershire, the earthen ramparts of the settlement were attacked, 
+        when the farmers were first competing for limited resources. Crickley Hill is the earliset know battle that took place in what is now the UK. At Crickley Hill in Gloucestershire, the earthen ramparts of the settlement were attacked, 
         some time around 3300 BC, and their defences partially levelled.`);
         map.setZoom(9);
         let crickleyCoords = {"lat": 51.8472, "lng": -2.0978};
@@ -164,9 +166,9 @@ function myMap() {
         whilst waiting for the long promised relief forces from Ireland, Scotland and France. Sir Jacob Astley took command of the Royalist forces in the west 
         and began to gather up the remnants from the handful of Royalist garrisons still left in the West Midlands to create a new field army. At this point 
         in the war, Royalist morale was low. However, Astley, a stalwart of the Royalist commanders and an experienced soldier, was able to cobble 
-        together a force of 3,000. While returning to Oxford he was intercepted and defeated at Stow-on-the-Wold. Some units of parliamentarian soldiers, arranged in regiments, 
-        wore uniforms coloured in Venetian Red - this was one of the first wars in which soldiers wore uniform clothing. In the years that followed, this colour would be 
-        adopted by all regiments of England's - and later Britain's - army, giving rise to the term 'redcoats.'`);
+        together a force of 3,000. While returning to Oxford he was intercepted and defeated at Stow-on-the-Wold, which was the last major battle of the first English Civil War. 
+        Some units of parliamentarian soldiers, arranged in regiments, wore uniforms coloured in Venetian Red - this was one of the first wars in which soldiers wore uniform clothing. 
+        In the years that followed, this colour would be adopted by all regiments of England's - and later Britain's - army, giving rise to the term 'redcoats.'`);
         map.setZoom(9);
         let stowCoords = {"lat": 51.951708, "lng": -1.728297};
         map.panTo(stowCoords);
@@ -197,7 +199,7 @@ function myMap() {
         2nd Viscount Lisle and William Berkeley, 2nd Baron Berkeley. It is notable for being the last battle fought in England entirely between the private armies of 
         feudal magnates. Lisle and Berkeley had long been engaged in a dispute over the inheritance of Berkeley Castle and the other Berkeley lands, Lisle being heir-general 
         to Thomas de Berkeley, 5th Baron Berkeley and Berkeley heir-male. Lisle impetuously challenged Berkeley to a battle, and the latter agreed, the battle to be fought the 
-        next day at Nibley Green. Lisle paid for his rashness with his life`);
+        next day at Nibley Green. Lisle paid for his rashness with his life.`);
         map.setZoom(9);
         let nibleyCoords = {"lat": 51.66, "lng": -2.3985};
         map.panTo(nibleyCoords);
