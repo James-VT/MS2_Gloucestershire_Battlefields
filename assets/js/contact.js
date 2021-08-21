04ffc7071b53b755e747ff61ba2b9985255ad580
James-VT <jd_tanner@hotmail.co.uk>
2021-08-21 11:44:15 +0000

    edit: Make name a required field in contact form

---
+++ assets/js/contact.js
@@ -0,0 +1,16 @@
+/** While building this file, I consulted this tutorial to refresh my memory: https://www.youtube.com/watch?v=In0nB0ABaUk
+ * However, that tutorial was for building a log-in form. I have adapted it heavily for my own use.
+ */
+
+const fullName = document.getElementById("full-name");
+
+const form = document.getElementById("form");
+
+const errorElement = document.getElementById("error-messages");
+
+form.addEventListener("submit", (e) => {
+    let errorMessages = []
+    if (fullName.value === "" || fullName.value == null) {
+        errorMessages.push("Your name is required")
+    }
+})
